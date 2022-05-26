3aef33c38fb37d08a3e1677d73bfb5c371ab90f9
sbarrington <sarahbarrington6@gmail.com>
2022-05-25 21:57:43 -0700

    Initial data and image processors

--- 01 Pre-Processing/img_processors.py
+++ 01 Pre-Processing/img_processors.py
@@ -41,6 +41,24 @@ def showimage(myimage):
     
     return None
 
+def apply_hsv_filter(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([0, 0, 120])
+    upper_blue = np.array([180, 38, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    result = cv2.bitwise_and(img, img, mask=mask)
+    b, g, r = cv2.split(result)  
+    filter = g.copy()
+        
+    ret,mask = cv2.threshold(filter,0,255, 1)
+
+    img[ mask == 0] = 255
+
+    showimage(img)
+    remove_bkg(img)
+
+    return None
+
 
 def create_catch_trial(img, threshold=0.7):
     segmentor = SelfiSegmentation()
